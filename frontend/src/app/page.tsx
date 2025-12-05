8e4a59161018969f389a3f1e404c8718bef248ad
Vishal <sriram182719@gmail.com>
2025-12-05 23:47:07 +0530

    Add Card and Tabs UI components and integrate CryptoSidebar into the main page layout.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
+import CryptoSidebar from "@/components/creative-tim/blocks/crypto-sidebar";
+
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black">
-      {/* Fresh black page */}
+    <div className="min-h-screen bg-black flex">
+      <CryptoSidebar />
+      <div className="flex-1">
+        {/* Main content area */}
+      </div>
     </div>
   );
 }
